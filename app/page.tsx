04c0a4f35af859092061567115252a067937a9e1
BC1337 <brettcomrie@gmail.com>
2023-08-13 11:25:59 -0500

    added prisma post model + migrate + generate, posts now show comment details

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import axios from 'axios'
 import AddPost from  './components/AddPost'
 import { useQuery } from '@tanstack/react-query'
 import Post from './components/Post'
+import { PostType } from './types/Posts'
 
 // fetch all posts
 const allPosts = async() => {
@@ -17,7 +18,7 @@ const allPosts = async() => {
 }
 
 const page = () => {
-  const { data, error, isLoading } = useQuery({
+  const { data, error, isLoading } = useQuery<PostType[]>({
     queryFn: allPosts,
     queryKey: ["posts"],
   }) 
@@ -37,6 +38,7 @@ const page = () => {
       <AddPost />
       {data?.map((post) => (
         <Post 
+          comments={post.comments}
           key={post.id}
           name={post.user.name}
           avatar={post.user.image}
