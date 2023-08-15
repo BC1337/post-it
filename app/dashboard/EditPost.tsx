ae08400d40ca3adb995f9fbd26db9de8146b4509
BC1337 <brettcomrie@gmail.com>
2023-08-15 11:34:25 -0500

    added frontend for deletePost, need to hook up backend now

--- app/dashboard/EditPost.tsx
+++ app/dashboard/EditPost.tsx
@@ -2,6 +2,9 @@
 
 import Image from "next/image"
 import { useState } from "react"
+import Toggle from "./Toggle"
+import { useMutation } from "@tanstack/react-query"
+import axios from "axios"
 
 type EditProps = {
     id: string
@@ -16,7 +19,28 @@ type EditProps = {
 }
 
 export default function EditPost({avatar, name, title, comments, id}: EditProps){
+    // Toggle
+    const [toggle, setToggle] = useState(false)
+    // Delete post
+    const {mutate} = useMutation(
+        async (id: string) => await axios.delete('/api/posts/deletePost', { data: id }),
+        {
+            onError: (error) => {
+                console.log(error)
+            },
+            onSuccess: (data) => {
+                console.log(data)
+            },
+        }
+  
+    )
+
+    const deletePost = () => {
+        mutate(id)
+    }
+    
     return(
+        <>
         <div className="bg-white my-8 p-8 rounded-lg">
             <div className="flex items-center gap-2">
                 <Image width={32} height={32} src={avatar} alt="avatar"/>
@@ -29,8 +53,16 @@ export default function EditPost({avatar, name, title, comments, id}: EditProps)
                 <p className="text-sm font-bold text-gray-700">
                     {comments?.length} Comments
                 </p>
-                <button className="text-sm font-bold text-red-500">Delete</button>
+                <button onClick={((e) => {
+                    setToggle(true)
+                })} 
+                  className="text-sm font-bold text-red-500"
+                >
+                    Delete
+                </button>
             </div>
         </div>
+        {toggle && <Toggle deletePost={deletePost} setToggle={setToggle}/>}
+        </>
     )
 }
