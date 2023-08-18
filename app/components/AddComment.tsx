8e6b0bf9bd1fbbf9e72b459b522f1d627a0db16f
BC1337 <brettcomrie@gmail.com>
2023-08-18 18:37:08 -0500

    added comment frontend, just need to make server side for it and pretty much done

---
+++ app/components/AddComment.tsx
@@ -0,0 +1,75 @@
+'use client'
+
+import { useState } from "react"
+import {useMutation, useQueryClient} from '@tanstack/react-query'
+import axios, { AxiosError } from "axios"
+import toast from 'react-hot-toast'
+
+type PostProps = {
+    id?: string
+}
+
+type Comment = {
+    postId?: string
+    title: string
+}
+
+export default function AddComment({id} : PostProps){
+    const [title, setTitle] = useState('')
+    const [isDisabled, setIsDisabled] = useState(false)
+    const queryClient = useQueryClient()
+    let commentToastId: string
+
+    const {mutate} = useMutation(
+        async (data: Comment) => axios.post('/api/posts/addComment', {data}), 
+        {
+            onSuccess: data => {
+                setTitle("")
+                setIsDisabled(false)
+                toast.success("Added your comment!", { id: commentToastId })
+            },
+            onError: (error) => {
+                setIsDisabled(false)
+                if (error instanceof AxiosError) {
+                    toast.error(error?.response?.data.message, { id: commentToastId })
+                }
+            }
+        }
+    )
+
+    const submitComment = async (e: React.FormEvent) => {
+        e.preventDefault()
+        setIsDisabled(true)
+        commentToastId = toast.loading("Adding your comment!", { id: commentToastId })
+        mutate({title, postId: id})
+    }
+
+    return(
+        <form className="my-8" >
+            <h3>Add a comment</h3>
+            <div className="flex flex-col my-2">
+                <input
+                 type="text"
+                 onChange={(e) => setTitle(e.target.value)}
+                 value={title}
+                 name="title"
+                 className="p-4 text-lg rounded-md my-2"
+                />
+            </div>
+            <div className="flex items-center gap-2">
+                <button
+                  disabled={isDisabled}
+                  className="text-sm bg-teal-600 text-white py-2 px-6 rounded-xl disabled:opacity-25"
+                  type="submit"
+                >
+                    Add Comment <span style={{ fontSize: '2rem' }}>🚀</span>
+                </button>
+                <p
+                 className={`font bold ${
+                    title.length > 300 ? "text-red-700" : "text-gray-400"
+                 }`}
+                >{`${title.length}/300`} </p>
+            </div>
+        </form>
+    )
+}
---
+++ app/post/[slug]/page.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import Post from "@/app/components/Post"
+import { PostType } from "@/app/types/Post"
+import { useQuery } from "@tanstack/react-query"
+import AddComment from "@/app/components/AddComment"
+import axios from "axios"
+
+type URL = {
+    params: {
+        slug: string
+    }
+}
+
+// fetch ALL posts
+const fetchDetails = async (slug: string) => {
+    const response = await axios.get(`/api/posts/${slug}`)
+    return response.data
+}
+
+
+export default function PostDetail(url: URL){
+    const {data, isLoading} = useQuery<PostType[]>({
+        queryKey: ['detail-posts'],
+        queryFn: () => fetchDetails(url.params.slug)
+    })
+    if(isLoading) return "Loading..."
+    console.log(data)
+    return(
+        <div>
+            <Post
+                id={data?.id}
+                name={data?.user.name}
+                avatar={data?.user.image}
+                postTitle={data?.title}
+                comments={data?.comments}
+        />
+        <AddComment id={data?.id}/>
+        </div>
+    )
+}
